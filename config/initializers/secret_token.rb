fed9300ea943c00036e4c604c9319d00732bd4ae
Zach Denton <zacharydenton@gmail.com>
2011-11-01 19:04:25 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blenderfiles::Application.config.secret_token = 'becb915a7af92af09615bc147e4d5ad63bf13d7afb4da6d8dd3c9d8982f9cf9f4c983fe41bd24e77394893aa6764d4f8f909cd6ba2f6966729685d682271ac60'
