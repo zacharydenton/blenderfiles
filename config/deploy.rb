f74afc849f311d28838c2d2e872b963312ac2dc5
Zach Denton <zacharydenton@gmail.com>
2012-01-17 18:42:33 -0500

    capistrano deployment

---
+++ config/deploy.rb
@@ -0,0 +1,40 @@
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require 'rvm/capistrano'
+require 'bundler/capistrano'
+
+set :application, "blenderfiles"
+set :repository,  "git@bitbucket.org:zacharydenton/blenderfiles.git"
+
+set :deploy_to, "/var/www/blenderfiles.org"
+set :user, "deploy"
+set :use_sudo, false
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "blenderfiles.org"                          # Your HTTP server, Apache/etc
+role :app, "blenderfiles.org"                          # This may be the same as your `Web` server
+role :db,  "blenderfiles.org", :primary => true # This is where Rails migrations will run
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+namespace :db do
+    desc <<-DESC
+      [internal] Updates the symlink for database.yml file to the just deployed release.
+    DESC
+    task :symlink, :except => { :no_release => true } do
+      run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    end
+
+  after "deploy:finalize_update", "db:symlink"
+end
